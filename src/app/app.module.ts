5e6bedc151a8903d0bc3cbf0c0380f955ed28f9e
samzerox <sam.zerox88@gmail.com>
2018-06-01 17:30:13 -0500

    Aplicacion de mapas funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+
+import { MapaComponent } from './components/mapa/mapa.component';
+import { MapaEditarComponent } from './components/mapa/mapa-editar.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+// maps
+import { AgmCoreModule } from '@agm/core';
+
+
+
 @NgModule({
+  entryComponents: [
+    MapaEditarComponent
+  ],
   declarations: [
-    AppComponent
+    AppComponent,
+    MapaComponent,
+    MapaEditarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBR_ZCk3JeNJA6bRMdIZmWzIoAuBi-bgiI'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/classes/marcador.class.ts
@@ -0,0 +1,14 @@
+
+export class Marcador {
+
+    public lat: number;
+    public lng: number;
+
+    public titulo = 'Sin titulo';
+    public desc = 'Sin descripcion';
+
+    constructor( lat: number, lng: number ) {
+        this.lat = lat;
+        this.lng = lng;
+    }
+}
